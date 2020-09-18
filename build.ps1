09ba055da82e0a5237d4ad8fb7f7da8426ba8f62
Hayden K <hay-kot@pm.me>
2020-09-18 15:39:01 -0800

    Updated auto build script

--- build.ps1
+++ build.ps1
@@ -1,7 +1,19 @@
 ./venv/Scripts/Activate.ps1
 
+$Version = "v0.32"
+$Name = "Visio2PDF $Version"
+$BuildDir = "./dist"
+$ArchivePath = "$BuildDir/$Name"
+
+
 pyinstaller --noconfirm --windowed `
+    --name $Name `
     --add-data="./Visio2PDF/web/;./web" `
     --add-binary="./Visio2PDF/OfficeToPDF.exe;." `
     --icon="./Visio2PDF/web/favicon.ico" `
     ./Visio2PDF/Visio2PDF.py
+
+Write-Host "Compressing to Archive..."
+Compress-Archive -Path $ArchivePath -DestinationPath "$ArchivePath.zip" -Force
+Write-Host "Archive Complete"
+
